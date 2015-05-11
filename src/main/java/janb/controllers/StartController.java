93c126564d36adf0eb5bc7b8118ae812f36536d8
Michael Anderson <drmikeando@gmail.com>
2015-05-11 17:57:31 +0800

    Trying to start on a start page.

---
+++ src/main/java/janb/controllers/StartController.java
@@ -0,0 +1,16 @@
+package janb.controllers;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by michaelanderson on 11/05/2015.
+ */
+public class StartController {
+    public void newProject(ActionEvent actionEvent) {
+        System.err.printf("StartController.newProject()\n");
+    }
+
+    public void openProject(ActionEvent actionEvent) {
+        System.err.printf("StartController.newProject()\n");
+    }
+}
